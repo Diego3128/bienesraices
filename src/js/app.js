5193860a28c990e45d1ce202ba801b720ca0ce59
DiegoFabian <145170018+Diego3128@users.noreply.github.com>
2024-09-18 20:42:15 -0500

    burguer menu animation

--- src/js/app.js
+++ src/js/app.js
@@ -6,8 +6,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function burguerMenu() {
     const navigation = document.querySelector('.bar .navigation');
+    const burguerMenuElement = document.querySelector('.burguer-icon');
 
     navigation.classList.toggle('show')
+    burguerMenuElement.classList.toggle('rotate');
 }
 function eventListeners() {
     const mobileMenu = document.querySelector('.mobile-menu');
