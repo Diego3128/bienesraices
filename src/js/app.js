55268ac9e4d21a523e43a4d5c2fdb21693ac4c19
DiegoFabian <145170018+Diego3128@users.noreply.github.com>
2024-09-18 19:36:47 -0500

    burguer menu logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,16 @@
-console.log('from ./src/js/app.js');
-console.log('cheese and potatoes');
+document.addEventListener('DOMContentLoaded', function () {
+    eventListeners();
+});
+
+
+
+function burguerMenu() {
+    const navigation = document.querySelector('.bar .navigation');
+
+    navigation.classList.toggle('show')
+}
+function eventListeners() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    mobileMenu.addEventListener('click', burguerMenu);
+}
