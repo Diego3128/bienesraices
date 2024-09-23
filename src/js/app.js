5380ea0a7d66a99d0e173a08ae9450fabb59bb0a
DiegoFabian <145170018+Diego3128@users.noreply.github.com>
2024-09-23 16:35:13 -0500

    dark mode based on user's device settings check user settings using window.watchMedia and then set the mode of user's device save preferences in localstorage if the user changes the default value.

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
     eventListeners();
+    userPreferences();
 });
 
+
+function userPreferences() {
+    // check if the user has already declare a mode, otherwise use their system preferences
+    if (localStorage.getItem('colorMode')) {
+        const colorMode = localStorage.getItem('colorMode');
+        document.body.classList.add(colorMode);
+        return;
+    }
+
+    const changeMode = (e) => {
+        //console.log(e);//MediaQueryListEvent
+        if (e.matches) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+            document.body.classList.add('light-mode');
+        }
+    }
+    // check system preferences
+    const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    darkModeQuery.addEventListener('change', changeMode);
+    // call right away to check the current state || if it matches dark then apply the style
+    changeMode(darkModeQuery);
+}
 
 function darkMode() {
     // toggle class 'dark-mode' to the body element
     document.body.classList.toggle('dark-mode');
+    //save the setting in the locol storage to future check with userPreferences function..
+    localStorage.setItem('colorMode', document.body.classList.contains('dark-mode') ? 'dark-mode' : 'light-mode');
 }
 
 function burguerMenu() {
@@ -18,7 +45,6 @@ function burguerMenu() {
 function eventListeners() {
     const mobileMenu = document.querySelector('.mobile-menu');
     const darkModeIcon = document.querySelector('.dark-mode-btn');
-
 
     mobileMenu.addEventListener('click', burguerMenu);
     darkModeIcon.addEventListener('click', darkMode);
