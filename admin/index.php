7be36ff564769bc480a3ccdc543b8a2fc49142fa
DiegoFabian <145170018+Diego3128@users.noreply.github.com>
2024-10-01 16:42:54 -0500

    property table styles

--- admin/index.php
+++ admin/index.php
@@ -14,6 +14,32 @@ $creaded = intval($creaded, 10);
     <?php endif; ?>
 
     <a href="/admin/properties/create.php" class="btnSmall btnSmall--green">Crear</a>
+
+    <table class="properties">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Titulo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td>111</td>
+                <td>titulooo</td>
+                <td><img src="/images/05a4669c0af9203f14d912020f7bf030.jpg" class="table-img" alt="property image"></td>
+                <td>100</td>
+                <td>
+                    <a href="#" class="btnLarge btnLarge--red">Eliminar</a>
+                    <a href="#" class="btnLarge btnLarge--blue">Actualizar</a>
+                </td>
+            </tr>
+        </tbody>
+
+    </table>
 </main>
 
 <?php
