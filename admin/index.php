6723d5307ba29ace09e80921d7d94f27eed6f420
DiegoFabian <145170018+Diego3128@users.noreply.github.com>
2024-10-01 17:19:22 -0500

    bring properties to the admin panel

--- admin/index.php
+++ admin/index.php
@@ -1,9 +1,26 @@
 <?php
+// Shows conditional alert message if a new property was created
+$creaded = $_GET["created"] ?? null;
+$creaded = intval($creaded, 10);
+
+// get properties from database
+
+// import conexion
+require '../includes/config/database.php';
+
+//create an instance of the conexion
+$db = connectToDB();
+
+// sql query (bring all properties)
+$propertySqlQuery = "SELECT * FROM propiedades";
+
+//do the query to the db
+$propertyResult = mysqli_query($db, $propertySqlQuery);
+
+// include header template
 require '../includes/functions.php';
 includeTemplate(templateName: 'header');
 
-$creaded = $_GET["created"] ?? null;
-$creaded = intval($creaded, 10);
 ?>
 
 <main class="container section">
@@ -15,33 +32,41 @@ $creaded = intval($creaded, 10);
 
     <a href="/admin/properties/create.php" class="btnSmall btnSmall--green">Crear</a>
 
-    <table class="properties">
-        <thead>
-            <tr>
-                <th>Id</th>
-                <th>Titulo</th>
-                <th>Imagen</th>
-                <th>Precio</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
+    <div class="table-container">
+        <table class="properties">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
 
-        <tbody>
-            <tr>
-                <td>111</td>
-                <td>titulooo</td>
-                <td><img src="/images/05a4669c0af9203f14d912020f7bf030.jpg" class="table-img" alt="property image"></td>
-                <td>100</td>
-                <td>
-                    <a href="#" class="btnLarge btnLarge--red">Eliminar</a>
-                    <a href="#" class="btnLarge btnLarge--blue">Actualizar</a>
-                </td>
-            </tr>
-        </tbody>
+            <tbody><!-- show query results -->
+                <?php while ($property = mysqli_fetch_assoc($propertyResult)): ?>
+                    <tr>
+                        <td><?php echo $property["id"]; ?></td>
+                        <td><?php echo $property["titulo"]; ?></td>
+                        <td><img src="/images/<?php echo $property["imagen"]; ?>" class="table-img" alt="property image"></td>
+                        <td><?php echo $property["precio"]; ?></td>
+                        <td>
+                            <a href="#" class="btnLarge btnLarge--red">Eliminar</a>
+                            <a href="#" class="btnLarge btnLarge--blue">Actualizar</a>
+                        </td>
+                    </tr>
+                <?php endwhile ?>
+            </tbody>
 
-    </table>
+        </table>
+    </div>
+
 </main>
 
 <?php
+// close db conexion
+mysqli_close($db);
+
 includeTemplate(templateName: 'footer');
 ?>
