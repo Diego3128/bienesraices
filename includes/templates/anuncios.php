36403b71a498267b8aaddafff00af8188f340500
DiegoFabian <145170018+Diego3128@users.noreply.github.com>
2024-10-03 20:34:30 -0500

    read properties from main page (CRUD)

---
+++ includes/templates/anuncios.php
@@ -0,0 +1,66 @@
+<?php
+
+//PATH RELATIVE TO THE INDEX.PHP FILE// It's the file originally including everything
+
+require "includes/config/database.php";
+
+$db = connectToDB();
+
+$query = "SELECT * FROM propiedades LIMIT {$numProperties}";
+
+
+$queryResult = mysqli_query($db, $query);
+
+// echo "<pre>";
+// while ($r = mysqli_fetch_assoc($queryResult)):
+//     var_dump($r);
+// endwhile;
+// echo "</pre>";
+
+?>
+
+
+<div class="ads-container">
+    <?php while ($row = mysqli_fetch_assoc($queryResult)): ?>
+        <div class="card">
+            <picture>
+                <img loading="lazy" src="/images/<?php echo $row["imagen"] ?>" alt="Propiedad anuncio">
+            </picture>
+
+            <div class="card__details">
+                <h3 class="card__title"><?php echo $row["titulo"] ?></h3>
+                <p class="card__description"><?php echo $row["descripcion"] ?></p>
+                <p class="card__price"><span class="dollar">$</span> <?php echo $row["precio"] ?></p>
+
+                <ul class="card__icons">
+
+                    <li>
+                        <img src="./build/img/icono_wc.svg" alt="restroom icon">
+                        <p><?php echo $row["wc"] ?></p>
+                    </li>
+
+                    <li>
+                        <img src="./build/img/icono_estacionamiento.svg" alt="parking lot icon">
+                        <p><?php echo $row["estacionamiento"] ?></p>
+                    </li>
+
+                    <li>
+                        <img src="./build/img/icono_dormitorio.svg" alt="room icon">
+                        <p><?php echo $row["habitaciones"] ?></p>
+                    </li>
+
+                </ul>
+
+                <!-- card__icons -->
+            </div>
+            <!-- card__details -->
+            <a href="anuncio.php?id=<?php echo $row["id"] ?>" class="btnLarge btnLarge--yellow">Ver propiedad</a>
+        </div>
+        <!-- card -->
+    <?php endwhile  ?>
+
+</div>
+
+<?php
+mysqli_close($db);
+?>
